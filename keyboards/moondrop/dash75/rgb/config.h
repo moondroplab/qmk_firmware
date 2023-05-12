64d77b4fd6b0610858f6d64f2887849761cb5e7d
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2023-05-12 13:22:34 +1000

    Add Moondrop Dash75 RGB

---
+++ keyboards/moondrop/dash75/rgb/config.h
@@ -0,0 +1,24 @@
+// Copyright 2023 moondroplab (@moondroplab)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define USB_SUSPEND_WAKEUP_DELAY 5000
+
+#define HOLD_ON_OTHER_KEY_PRESS
+
+/* RGB */
+#define RGB_DI_PIN C7
+
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 200
+
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLED_NUM 81
+
+#    define RGBLIGHT_LIMIT_VAL 120 // The maximum brightness level
+#    define RGBLIGHT_SLEEP         // If defined, the RGB lighting will be switched off when the host goes to sleep */
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#    define RGBLIGHT_EFFECT_RGB_TEST
+#endif
